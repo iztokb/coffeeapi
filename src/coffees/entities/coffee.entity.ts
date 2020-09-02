8fffe3cd388f5af231049ff0f7410e6c7c701270
Iztok Berdnik <iztok.berdnik@rcg.si>
2020-09-02 06:12:59 +0200

    added typeorm

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 export class Coffee {
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavours: string[];
+
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
 }
